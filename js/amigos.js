361ac79be7b803874cb4f93ef475e52d4fd878d5
Ana Liotta <liotta.a.p@gmail.com>
2024-11-17 18:37:53 -0300

    Cambios visuales en el html

---
+++ js/amigos.js
@@ -0,0 +1,27 @@
+const Amigos = [
+    {
+        img: "./img/amigos_4"
+    },
+    {
+        img: "./img/amigos_5"
+    },
+    {
+        img: "./img/amigos_6"
+    },
+    {
+        img: "./img/amigos_7"
+    },
+    {
+        img: "./img/amigos_8"
+    },
+    {
+        img: "./img/amigos_9"
+    },
+    {
+        img: "./img/amigos_10"
+    },
+    {
+        img: "./img/amigos_12"
+    },
+
+]
